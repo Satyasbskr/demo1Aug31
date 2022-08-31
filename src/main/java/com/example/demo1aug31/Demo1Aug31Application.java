4e1413516243f0952b11134d07e722796acd2f70
Satyasbskr <79159922+Satyasbskr@users.noreply.github.com>
2022-08-31 11:31:15 -0500

    Commit done1

---
+++ src/main/java/com/example/demo1aug31/Demo1Aug31Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1aug31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Aug31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Aug31Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo1aug31/Demo1Aug31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1aug31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1Aug31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
